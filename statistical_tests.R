4ede6ff5a3d03f6dac56acdef9ed22de5bc38185
HaydenWaller <115991293+HaydenWaller@users.noreply.github.com>
2022-10-20 13:28:04 +0200

    Add files via upload

---
+++ statistical_tests.R
@@ -0,0 +1,20 @@
+#islands
+prop.test(x=c(1852, 2106), n=c(9850, 9850))
+
+
+#clustering_lk
+prop.test(x=c(1377, 246), n=c(9850, 1744))
+
+
+#clustering_lk
+prop.test(x=c(1377, 273), n=c(9850, 1960))
+
+
+#clustering_parallel
+prop.test(x=c(1377, 37), n=c(9850, 260))
+
+
+#Parallel Expression
+
+prop.test(x=c(271, 281), n=c(572, 572))
+prop.test(x=c(37, 41), n=c(78, 78))
